1df2a3d2d873b5450915939e6838ff11abb7a8fa
Charles Reid <charlesreid1@gmail.com>
2018-04-14 21:22:53 -0700

    pages use twitter blue

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'charlesreid1'
 
-SITENAME = u'Bots'
+SITENAME = u'charlesreid1 pages'
 
 SITEURL = ''
 
@@ -21,7 +21,142 @@ THEME = 'scurvy-knave-theme'
 # Pelican is designed for files => pages.
 # Use variables (below) to set pieces of pages.
 
-CTHULHU = "Cthulhu R'lyeh fhtagn"
+# pages use twitter blue
+INTROCOLOR  = "#fff"
+ACOLOR      = "#00aced"
+AHOVERCOLOR = "#0084b4"
+BRIGHTCOLOR = "#1dcaff"
+TEMPLATE_PAGES = {
+    'custom.css' : 'custom.css'
+}
+
+# ---
+
+# description appears between <p> tags, so don't include them
+
+SITE_TITLE = "pages.charlesreid1.com"
+SITE_DESCRIPTION = "a subdomain for charlesreid1 pages"
+
+# ---
+
+# include <p> tags in the description
+
+ABOUT_TITLE = "about pages.charlesreid1.com"
+
+ABOUT_DESCRIPTION = """
+<p>
+<a href="https://git.charlesreid1.com/bots">bots on git.charlesreid1.com</a>
+</p>
+
+<p>&nbsp;</p>
+
+<p><b>What is a bot?</b></p>
+
+<p>Broadly, a bot is an autonomous entity that executes a program.<br />
+The bots on this site are principally Twitter bot flocks.<br />
+Also see <a href="https://twitter.com/horse_ebooks">@horse_ebooks</a>.</p>
+
+<p>&nbsp;</p>
+
+<p><b>What is a bot flock?</b></p>
+
+<p>A bot flock is a group of Twitter bots that perform a related task,
+access related data, or otherwise share some structure.<br />
+See below for examples of Twitter bot flocks.</p>
+
+<p>&nbsp;</p>
+
+<p><b>Where can I find the bots?</b></p>
+
+<p>Each bot has a home page on bots.charlesreid1.com; see below for links.</p>
+"""
+
+# ---
+
+# include <p> tags in the description
+
+def make_links_description():
+    descr = ""
+
+    botlinks = {
+            'twitter' : {
+                'Apollo Space Junk Bot Flock' : 'https://twitter.com/charlesreid1/lists/space-junk-botflock',
+                'Paradise Lost Bot Flock' :     'https://twitter.com/charlesreid1/lists/miltonbotflock',
+                'Ginsberg Bot Flock' :          'https://twitter.com/charlesreid1/lists/ginsbergbotflock',
+                'Math Tripos Bot' :             'https://twitter.com/math_tripos'
+            },
+
+            'git.charlesreid1.com' : {
+                'Rainbow Mind Machine' :        'https://git.charlesreid1.com/bots/b-rainbow-mind-machine',
+                'Apollo Space Junk Bot Flock' : 'https://git.charlesreid1.com/bots/b-apollo',
+                'Paradise Lost Bot Flock' :     'https://git.charlesreid1.com/bots/b-milton',
+                'Ginsberg Bot Flock' :          'https://git.charlesreid1.com/bots/b-ginsberg',
+                'Math Tripos Bot' :             'https://git.charlesreid1.com/bots/b-tripos'
+            },
+
+            'pages.charlesreid1.com' : {
+                'Rainbow Mind Machine' :        'https://pages.charlesreid1.com/rainbow-mind-machine',
+                'Apollo Space Junk Bot Flock' : 'https://pages.charlesreid1.com/apollo',
+                'Paradise Lost Bot Flock' :     'https://pages.charlesreid1.com/milton',
+                'Ginsberg Bot Flock' :          'https://pages.charlesreid1.com/ginsberg',
+                'Math Tripos Bot' :             'https://pages.charlesreid1.com/tripos'
+            },
+
+            'github (mirror)' : {
+                'Rainbow Mind Machine' :        'https://github.com/charlesreid1/rainbow-mind-machine',
+                'Apollo Space Junk Bot Flock' : 'https://github.com/charlesreid1/apollospacejunk',
+                'Paradise Lost Bot Flock' :     'https://github.com/charlesreid1/milton',
+                'Ginsberg Bot Flock' :          'https://github.com/charlesreid1/ginsberg',
+                'Math Tripos Bot' :             'https://github.com/charlesreid1/tripos-bot'
+            },
+
+            'github pages (mirror)' : {
+                'Rainbow Mind Machine' :        'https://charlesreid1.github.io/rainbow-mind-machine',
+                'Apollo Space Junk Bot Flock' : 'https://charlesreid1.github.io/apollospacejunk',
+                'Paradise Lost Bot Flock' :     'https://charlesreid1.github.io/milton',
+                'Ginsberg Bot Flock' :          'https://charlesreid1.github.io/ginsberg',
+                'Math Tripos Bot' :             'https://charlesreid1.github.io/tripos-bot'
+            }
+
+    }
+
+    fa_icons = {
+            'twitter' : '<i class="fa fa-twitter fa-fw"></i>',
+            'git.charlesreid1.com' : '<i class="fa fa-code-fork fa-fw"></i>',
+            'pages.charlesreid1.com' : '<i class="fa fa-file-o fa-fw"></i>',
+            'github (mirror)' : '<i class="fa fa-github fa-fw"></i>',
+            'github pages (mirror)' : '<i class="fa fa-github-square fa-fw"></i>'
+    }
+
+    for key in botlinks.keys():
+        descr += "<h3>charlesreid1 bots on %s:<h3>\n\n"%(key)
+        fa_icon = fa_icons[key]
+
+        links = botlinks[key]
+        for bot_name in links.keys():
+            bot_link = links[bot_name]
+            descr += "<p><a class=\"btn btn-default btn-lg\" href=\"%s\">"%(bot_link)
+            descr += "%s %s"%(fa_icon, bot_name)
+            descr += "</a></p>\n"
+        
+        descr += "\n"
+
+    return descr
+
+LINKS_TITLE = "bot links"
+
+LINKS_DESCRIPTION = make_links_description()
+
+
+# ---
+
+
+CONTACT_TITLE = "Contact charlesreid1"
+
+CONTACT_DESCRIPTION = """<p>Get in touch!</p>
+<p><a href="mailto:charles@charlesreid1.com">charles (at) charlesreid1.com</a></p>
+"""
+
 
 # --------------8<---------------------
 
